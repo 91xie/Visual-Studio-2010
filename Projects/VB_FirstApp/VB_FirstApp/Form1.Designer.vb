43b1871161488ba6ec6804afc519154725d923f6
Patrick <110352329@umail.ucc.ie>
2012-08-12 14:51:55 +0100

    VB listbox fixed

--- Projects/VB_FirstApp/VB_FirstApp/Form1.Designer.vb
+++ Projects/VB_FirstApp/VB_FirstApp/Form1.Designer.vb
@@ -25,6 +25,7 @@ Partial Class Form1
         Me.Button1 = New System.Windows.Forms.Button()
         Me.ListBox1 = New System.Windows.Forms.ListBox()
         Me.ListBox2 = New System.Windows.Forms.ListBox()
+        Me.Label1 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'Button1
@@ -52,21 +53,33 @@ Partial Class Form1
         Me.ListBox2.Size = New System.Drawing.Size(106, 147)
         Me.ListBox2.TabIndex = 2
         '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(78, 254)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Label1"
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(427, 312)
+        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.ListBox2)
         Me.Controls.Add(Me.ListBox1)
         Me.Controls.Add(Me.Button1)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
     Friend WithEvents Button1 As System.Windows.Forms.Button
     Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
     Friend WithEvents ListBox2 As System.Windows.Forms.ListBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
 
 End Class
--- Projects/VB_FirstApp/VB_FirstApp/Form1.vb
+++ Projects/VB_FirstApp/VB_FirstApp/Form1.vb
@@ -1,22 +1,26 @@
 ﻿Public Class Form1
 
     Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-        Button1.Text = "NO"
+
+        Dim consolewrite
+        consolewrite = ""
+
+        Button1.Text = "Change"
+
+        Label1.Text = "Text"
 
         Dim options(5)
 
-
-
-        For Each x In options
-            Dim y
-            y = x
-            ListBox1.Items.Add(y)
+        For x = 0 To options.Length - 1
+            options(x) = "Option" + x.ToString()
         Next
 
-        Dim abitof
-        abitof = "hello"
-        ListBox1.Items.Add(abitof)
-        ListBox1.Refresh()
+        For Each x In options
+            ListBox1.Items.Add(x)
+        Next
+
+
+
 
     End Sub
 
@@ -25,7 +29,12 @@
 
 
     Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
-        ListBox1.Items.Add("another item")
+        If ListBox1.SelectedIndex > -1 Then
+            ListBox2.Items.Add(ListBox1.Items.Item(ListBox1.SelectedIndex))
+        End If
+
+
+
     End Sub
 
     
