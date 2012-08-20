757fe82dbde5a663af42b3069cd6ebf99c5fecf1
Patrick <110352329@umail.ucc.ie>
2012-08-21 00:38:35 +0100

    Log1 added

---
+++ Projects/Log1/Log1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Log1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        static string TextFileReader(string filepath)
+        {
+            System.IO.StreamReader Inputfile = new System.IO.StreamReader(filepath)
+            string s;
+            s = "";
+            while (!Inputfile.EndOfStream)
+            {s = s + Inputfile.ReadLine();}
+            return s;
+        }
+
+        static void TextFileWriter(string filepath, string text)
+        {
+            System.IO.StreamWriter theFile = new System.IO.StreamWriter(filepath)
+            theFile.Write(text);
+            theFile.Close();
+        }
+        
+    }
+
+
+}
